93cbccc0ba5f421d81ebf80582a8aaf426b1ab3e
lemical07 <icaleladio@gmail.com>
2026-09-15 17:28:48 -0600

    feat: add Teacher class extending Person

---
+++ src/models/teacher.js
@@ -0,0 +1,12 @@
+import { Person } from './person.js';
+
+export class Teacher extends Person {
+    #id;
+
+    constructor(id, firstName, lastName, identificationType, identificationNumber, email) {
+        super(firstName, lastName, identificationType, identificationNumber, email);
+        this.#id = id;
+    }
+
+    get id() { return this.#id; }
+}
